f72b484d4044676701f9ef499b1d20f8ccc336dd
serenaelena <117343345+serenaelena@users.noreply.github.com>
2023-06-28 22:26:30 +0300

    Update fire_train.py

--- training/fire_train.py
+++ training/fire_train.py
@@ -3,6 +3,6 @@ import os
 os.environ["CUDA_LAUNCH_BLOCKING"]="1"
 if __name__ == '__main__':
     model = YOLO("yolov8s.pt")  # load a pretrained model (recommended for training)
-    model.train(data="./data.yaml", epochs=300, imgsz=640, device='0')
+   model.train(data="./data.yaml", epochs=300, imgsz=640, device='0', workers=4)
    # model = YOLO("./runs/detect/train6/weights/best.pt")
    # model.val(data="data.yaml", imgsz=640, batch=16, conf=0.001, iou=0.6, save_json=True, model=model)
